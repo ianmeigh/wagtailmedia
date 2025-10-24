9dc23fe8d6a63e613ea9e13e51817d294663f6b2
Ian Meigh <ian.meigh@torchbox.com>
2025-10-24 12:40:52 +0100

    refactor: remove unused api endpoints in test urls

--- tests/testapp/urls_aws_webhook.py
+++ tests/testapp/urls_aws_webhook.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from wagtail.api.v2.router import WagtailAPIRouter
 
-from wagtailmedia.api.views import MediaAPIViewSet
 from wagtailmedia.views.aws_webhooks import AWSTranscodingWebhookView
 
-
-api_router = WagtailAPIRouter("wagtailapi_v2")
-api_router.register_endpoint("media", MediaAPIViewSet)
 
 urlpatterns = [
     path(
